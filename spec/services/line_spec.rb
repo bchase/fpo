a0640e45da742b5a8f30b22c4031e24d5bc4c0f7
Brad Chase <bosco87@gmail.com>
2014-02-22 14:35:18 -0500

    stub out Line service class

---
+++ spec/services/line_spec.rb
@@ -0,0 +1,15 @@
+require 'spec/spec_helper'
+
+describe Line do
+  describe '.initialize' do
+  end
+
+  describe '#to_s' do
+  end
+
+  describe '#inspect' do
+  end
+
+  describe '#each' do
+  end
+end
