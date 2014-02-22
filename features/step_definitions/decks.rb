96ad4de311643ed9aa9c58a00bdaea904a06603b
Brad Chase <bosco87@gmail.com>
2014-02-21 20:33:29 -0500

    comment out step defs

--- features/step_definitions/decks.rb
+++ features/step_definitions/decks.rb
@@ -1,47 +1,47 @@
-Given(/^I visit the new Deck page$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Given(/^I select "(.*?)" from "(.*?)"$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I fill in "(.*?)" with the song name$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I fill in "(.*?)" with the artist's name$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I fill in "(.*?)" with the lyrics$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I press "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I should see the song name$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^there should be a new Deck$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Given(/^I have a populated Deck$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I navigate to that Deck$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^click "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I should see that Deck as an Anki export$/) do
-  pending # express the regexp above with the code you wish you had
-end
+# Given(/^I visit the new Deck page$/) do
+#   pending # express the regexp above with the code you wish you had
+# end
+# 
+# Given(/^I select "(.*?)" from "(.*?)"$/) do |arg1, arg2|
+#   pending # express the regexp above with the code you wish you had
+# end
+# 
+# When(/^I fill in "(.*?)" with the song name$/) do |arg1|
+#   pending # express the regexp above with the code you wish you had
+# end
+# 
+# When(/^I fill in "(.*?)" with the artist's name$/) do |arg1|
+#   pending # express the regexp above with the code you wish you had
+# end
+# 
+# When(/^I fill in "(.*?)" with the lyrics$/) do |arg1|
+#   pending # express the regexp above with the code you wish you had
+# end
+# 
+# When(/^I press "(.*?)"$/) do |arg1|
+#   pending # express the regexp above with the code you wish you had
+# end
+# 
+# Then(/^I should see the song name$/) do
+#   pending # express the regexp above with the code you wish you had
+# end
+# 
+# Then(/^there should be a new Deck$/) do
+#   pending # express the regexp above with the code you wish you had
+# end
+# 
+# Given(/^I have a populated Deck$/) do
+#   pending # express the regexp above with the code you wish you had
+# end
+# 
+# When(/^I navigate to that Deck$/) do
+#   pending # express the regexp above with the code you wish you had
+# end
+# 
+# When(/^click "(.*?)"$/) do |arg1|
+#   pending # express the regexp above with the code you wish you had
+# end
+# 
+# Then(/^I should see that Deck as an Anki export$/) do
+#   pending # express the regexp above with the code you wish you had
+# end
