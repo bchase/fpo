facc1a97d7d5e7e51ba045c121d7741b7d8d9857
Brad Chase <bosco87@gmail.com>
2014-02-22 15:04:32 -0500

    Line#initialize

---
+++ app/services/line.rb
@@ -0,0 +1,21 @@
+class Line < String
+  def initialize(record, line_num)
+    text = text_for(record)
+    line = text.hanzi_line(line_num)
+    super line
+    freeze
+  end
+
+private
+  def text_for(record)
+    case record
+    when Deck
+      deck = record
+      deck.text
+    when Text
+      text = record
+    else
+      raise TypeError.new "not a valid record class: #{record.class} for #{record.inspect}"
+    end
+  end
+end
--- spec/services/line_spec.rb
+++ spec/services/line_spec.rb
@@ -1,7 +1,32 @@
-require 'spec/spec_helper'
+require 'spec_helper'
 
 describe Line do
+  # TODO 
+  #   dup'd from text_spec...
+  let(:song_text) { create :text }
+
+  let(:line_one) { song_text.raw.split("\n").first }
+  let(:line_two) { song_text.raw.split("\n").second }
+  let(:hanzi_line_two) { song_text.raw.split("\n").third }
+
+  let(:deck)      { create :deck, text: song_text }
+
+  let(:meishuode) { create :meishuode }
+  let(:mei)       { create :mei }
+  let(:mo)        { create :mo }
+
   describe '.initialize' do
+    it 'takes a deck and a line number (for deck text)' do
+      line = Line.new deck, 1
+
+      line.should eq(line_one)
+    end
+
+    it 'takes a text and a line number' do
+      line = Line.new song_text, 1
+
+      line.should eq(line_one)
+    end
   end
 
   describe '#to_s' do
