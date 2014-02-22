f1bc5a34238bff7f366282c5536620be33894e9a
Brad Chase <bosco87@gmail.com>
2014-02-22 11:17:45 -0500

    include FactoryGirl methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # include FactoryGirl methods (e.g. `create`, `build`)
+  config.include FactoryGirl::Syntax::Methods
 end
