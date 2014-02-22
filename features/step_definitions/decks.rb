0561fb723e03ffa780fbcde9a497061b6f617ed7
Brad Chase <bosco87@gmail.com>
2014-02-21 19:51:07 -0500

    comment out step defs contents

--- features/step_definitions/decks.rb
+++ features/step_definitions/decks.rb
@@ -1,11 +1,11 @@
-Song < Struct.new(:name, :artist, :lyrics)
-
-# name = "Shan Qiu - Jonathan Lee (山丘 - 李宗盛)"
-name   = "山丘"
-artist = "Jonathan Lee (李宗盛)"
-lyrics = "想說卻還沒說的 還很多
-Xiang shuo que hai mei shuo de hai hen duo
-攢着是因為想寫成歌
-Zan zhe shin yin wei xiang xie cheng ge"
-
-@song  = Song.new name, artist, lyrics
+# Song < Struct.new(:name, :artist, :lyrics)
+# 
+# # name = "Shan Qiu - Jonathan Lee (山丘 - 李宗盛)"
+# name   = "山丘"
+# artist = "Jonathan Lee (李宗盛)"
+# lyrics = "想說卻還沒說的 還很多
+# Xiang shuo que hai mei shuo de hai hen duo
+# 攢着是因為想寫成歌
+# Zan zhe shin yin wei xiang xie cheng ge"
+# 
+# @song  = Song.new name, artist, lyrics
